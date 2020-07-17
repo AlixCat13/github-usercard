b239a271a29bfe61aa3766e4d3c0f80e4ec2b037
Cody C <alix.henry@ymail.com>
2020-07-16 21:23:35 -0500

    getting things set up

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -58,3 +58,35 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
+
+axios.get("https://api.github.com/users/AlixCat13")
+.then((successResponse) => {
+  console.log(successResponse)
+  return successResponse
+})
+
+.catch((errorResponse) => {
+  console.log(errorResponse)
+})
+
+const gitCard = (userProfile) => {
+  const card = document.createElement('div')
+  const userPic = document.createElement('img')
+  const cardInfo = document.createElement('p')
+  const name = document.createElement('h3')
+  const userName = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const link = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+  userName.classList.add('username')
+
+  
+}
